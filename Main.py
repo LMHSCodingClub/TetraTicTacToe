c0d7e1f28f04ee9db7a475d87a1a005c27712d07
Phillip Grabovsky <phillip.grabovsky@gmail.com>
2019-11-11 15:08:50 -0500

    finished main function, simulated checkwin and input functions

--- Main.py
+++ Main.py
@@ -1,25 +1,39 @@
-import checkWin
+#import checkWin
+
 
 player = 1
-board = createEmptyBoard()
+move = 0
 
-while not checkWin():
-    player *= -1
-    cube = getUserInput()
-    board[cube[0]][cube[1]][cube[2]] = player
-    print(board)
+def checkWin(move):
+    if move < 3:
+        return False
+    else:
+        return True
 
 def createEmptyBoard():
     print("ok")
     board = []
-    for(i in range(4)):
+    for i in range(4):
         empty = []
         board.append(empty)
-        for(j in range(4)):
+        for j in range(4):
             emptyDim2 = []
             board[i].append(emptyDim2)
-            for(k in range(4)):
+            for k in range(4):
                 board[i][j].append(0)
 
     return board
-                
+
+def getUserInput(move):
+    return [move,move,move]
+
+board = createEmptyBoard()
+
+while not checkWin(move):
+    move+=1
+    player *= -1
+    cube = getUserInput(move)
+    board[cube[0]][cube[1]][cube[2]] = player
+    print(board)
+
+print("Player " + str(player)  + " won!")
