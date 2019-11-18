11108c9365ecb33e3250ee6d40a77c7b813ac417
Aviel Resnick <aviel.resnick@gmail.com>
2019-11-18 15:37:03 -0500

    Implemented get color value at x,y

--- User.py
+++ User.py
@@ -1,2 +1,11 @@
+from PIL import Image
+
+'''
 def getUserInput(move):
     return [move,move,move]
+'''
+
+colorMap = Image.open('gameboardColor.png') # This is the colormap, we never want the user to see it
+pix = colorMap.load()
+value = pix[300,300][:3] # returns color value at x,y
+print(value)
