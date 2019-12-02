62d7be30af30995440cb7ba0c8f93db678cf93ae
Phillip Grabovsky <phillip.grabovsky@gmail.com>
2019-12-02 15:05:06 -0500

    Updated interface - linked to user and checkWin functions

--- interface.py
+++ interface.py
@@ -23,29 +23,34 @@ def createEmptyBoard():
 LEFT = 1 # left button
 (width, height) = (600, 750)
 backgroundColor = (255,255,255)
-bg = pygame.image.load('gameboard.png')
+bg = pygame.image.load('/home/phillip/projects/TetraTicTacToe/gameboard.png')
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()# screen modifications must be done before flip()
 
 #game definitions
 player = 1
 move = 0
+cube = [0,0,0]
 board = createEmptyBoard()
 
 
-while not CheckWin.checkWin(move, board):
+while not CheckWin.checkWin(cube, board, player):
     #update move stuff
-    move+=1
-    player *= -1
+    move+=1 #move count
+    player *= -1 #changes player
 
     #update screen
     screen.blit(bg, (0, 0))
     pygame.display.flip()
 
-    #wait for a click, quit when X clicked
+    #wait for a click
     event = pygame.event.poll()
+
+    #if the red x is clicked
     if event.type == pygame.QUIT:
         pygame.quit()
+
+    #if you left click
     elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
         print ("You pressed the left mouse button at (%d, %d)" % event.pos)
     elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
@@ -55,7 +60,9 @@ while not CheckWin.checkWin(move, board):
     #now that user input has been detected, play the game
     cube = User.getUserInput(event.pos)
     board[cube[0]][cube[1]][cube[2]] = player
-    print(board)
+    updateBoard()
 
 
-print("Player " + str(player)  + " won!")
+
+def updateBoard():
+    print("Player " + str(player)  + " won!")
