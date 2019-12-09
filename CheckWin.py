056bacc8aea005fec277630b18975c143aca980e
David-Volchonok <57112433+David-Volchonok@users.noreply.github.com>
2019-12-09 15:46:52 -0500

    Colons added

--- CheckWin.py
+++ CheckWin.py
@@ -16,56 +16,56 @@ def checkWin(move, board, player): # move: [x, y, z] AKA the address
        return True
 
    # x-check
-   if check(lambda i: i, lambda i: y, lambda i: z)
+   if check(lambda i: i, lambda i: y, lambda i: z):
       return True
    
    # y-check
-   if check(lambda i: x, lambda i: i, lambda i: z)
+   if check(lambda i: x, lambda i: i, lambda i: z):
       return True
    
    # diagonal check 1
-   if check(lambda i: i, lambda i: i, lambda i: z)
+   if check(lambda i: i, lambda i: i, lambda i: z):
       return True
    
    # diagonal check 2
-   if check(lambda i: i, lambda i: 3 - i, lambda i: z)
+   if check(lambda i: i, lambda i: 3 - i, lambda i: z):
       return True
    
    # diagonal check 3
-   if check(lambda i: x, lambda i: i, lambda i: i)
+   if check(lambda i: x, lambda i: i, lambda i: i):
       return True
 
    # diagonal check 4
-   if check(lambda i: x, lambda i: 3 - i, lambda i: i)
+   if check(lambda i: x, lambda i: 3 - i, lambda i: i):
       return True
    
    
    # diagonal check 5
-   if check(lambda i: i, lambda i: y, lambda i: i)
+   if check(lambda i: i, lambda i: y, lambda i: i):
       return True
 
    
    # diagonal check 6
-   if check(lambda i: 3 - i, lambda i: y, lambda i: i)
+   if check(lambda i: 3 - i, lambda i: y, lambda i: i):
       return True
   
    #Grand Diagonal Checks (4)
 
 
    #GD check 1
-   if check(lambda i: i, lambda i: i, lambda i: i)
+   if check(lambda i: i, lambda i: i, lambda i: i):
       return True
 
    #GD check 2
-   if check(lambda i: 3 - i, lambda i: i, lambda i: i)
+   if check(lambda i: 3 - i, lambda i: i, lambda i: i):
       return True
 
    #GD check 3
-   if check(lambda i: i, lambda i: 3 - i, lambda i: i)
+   if check(lambda i: i, lambda i: 3 - i, lambda i: i):
       return True
 
    #GD check 4
-   if check(lambda i: i, lambda i: i, lambda i: 3 - i)
+   if check(lambda i: i, lambda i: i, lambda i: 3 - i):
       return True
 
    return False
